6e5416cc8638cd6a122c816f6b50428569e69f91
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-04-27 22:22:43 +0100

    Bug fix

--- webapp.py
+++ webapp.py
@@ -9,9 +9,9 @@ def add_todo():
     functions.write_todos(todos)
 
 
-st.title('My Todo App')
+st.title('My Todo App') #app title 
 st.subheader('Get Task Done Quickly!')
-st.text_input(label='', placeholder='Enter a todo..',
+st.text_input(label='', placeholder='Enter a  todo....',
               on_change=add_todo, key='new_todo')
 for index, todo in enumerate(todos):
     check_box = st.checkbox(todo, key=todo)
